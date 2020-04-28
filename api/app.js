8896b5f216a7f59cd337e630bc611b52969b8686
AngelFFigueroaRivera <angfir@live.com.mx>
2020-04-27 21:23:00 -0500

    cambios

--- api/app.js
+++ api/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
 var mapRouter = require('./routes/map');
+var regionRouter = require('./routes/region');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
 app.use('/map', mapRouter);
+app.use('/region', regionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
