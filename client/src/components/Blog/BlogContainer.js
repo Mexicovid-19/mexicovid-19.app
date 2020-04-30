c7b32149d9cfe4f8fb3d32b3d34c6a1a11304ea9
SeaWar741 <juanca741@gmail.com>
2020-04-29 22:28:11 -0500

    complete display on research, continue fixing css and migrate to better markdown to html

--- client/src/components/Blog/BlogContainer.js
+++ client/src/components/Blog/BlogContainer.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const BlogContainer = styled.div`
-  margin: 2rem auto 0;
+  margin: 5rem auto 0;
   width: 90%;
   max-width: 700px;
   padding: 20px 10px;
