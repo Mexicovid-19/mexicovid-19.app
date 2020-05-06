fb0e99b3f5461ddc77bc3921bdf5b51b804c1c27
Mildred Gil <mi.mitel@hotmail.com>
2020-05-05 20:24:18 -0500

    Updates

---
+++ precache-manifest.450f289c27337d2eb9465178fdd1ed43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1551cc36f1f1e690fd8547cc6b08959",
+    "url": "/mexicovid-19.app/index.html"
+  },
+  {
+    "revision": "54a650e225c53c541e99",
+    "url": "/mexicovid-19.app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "391fdd56198a16dde7b0",
+    "url": "/mexicovid-19.app/static/js/2.fd3cea10.chunk.js"
+  },
+  {
+    "revision": "885ae942ecae716357452294747962e0",
+    "url": "/mexicovid-19.app/static/js/2.fd3cea10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a650e225c53c541e99",
+    "url": "/mexicovid-19.app/static/js/main.02c74616.chunk.js"
+  },
+  {
+    "revision": "27fbaf428f2dfe1f7ecf",
+    "url": "/mexicovid-19.app/static/js/runtime-main.d75811e0.js"
+  }
+]);
