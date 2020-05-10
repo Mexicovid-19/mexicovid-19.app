1e18cd91f2ea39dd39d6dfc085fc599d2f74acf4
SeaWar741 <juanca741@gmail.com>
2020-05-10 03:26:14 -0500

    ya es PWA! (se añadio un service worker y mejoras para conseguir twitter card/facebook app id

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(
@@ -8,3 +9,7 @@ ReactDOM.render(
         <App/>
     </BrowserRouter>
 , document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
