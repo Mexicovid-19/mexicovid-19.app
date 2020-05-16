4f94f5094a1a099bf0195c5f8f78717611a7efbb
--replace-all <--replace-all>
2020-05-15 23:16:15 -0500

    Update before merge

--- api/config.js
+++ api/config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   masterKey: process.env.API_KEY,
-  port: process.env.PORT || 8000,
+  port: process.env.PORT || 8000, //Changed to 8000 because of internal error
   dbUrl: process.env.MONGO_DB_URL
-};
+}; 
