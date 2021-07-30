2f6a4007bc856f1f700c7030986824409159402d
Alex Vargas <alexjesusvargas@gmail.com>
2021-07-30 17:28:32 -0500

    changed modal info & design

--- client/src/components/Loteria/Modal.js
+++ client/src/components/Loteria/Modal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { CSSTransition } from "react-transition-group";
 import ReactDOM from "react-dom";
-import Semaforo from "./Semaforo.js"
+import Semaforo from "./Semaforo.js";
 import "./Modal.css";
 
 const Modal = (props) => {
@@ -16,26 +16,35 @@ const Modal = (props) => {
         onClick={props.onClose}
       >
         <div className="modal-content" onClick={(e) => e.stopPropagation}>
+          {/* div modal header starts */}
           <div className="modal-header">
-            <h4 className="modal-title">{props.title}</h4>
+            <h1 className="modal-title">{props.title}</h1>
+            <h2 className="modal-date">{props.dateOfChange}</h2>
           </div>
-          <div className="modal-body">{props.children}</div>
+          {/* div modal header ends */}
+
+          {/* div modal footer starts */}
           <div className="modal-footer">
+            {/* div semaforo starts */}
             <div className="semaforoActual">
-              <Semaforo color = {props.currentTrafficLight}/>
+              <Semaforo color={props.currentTrafficLight} />
+              <div className="semaforo-anterior">
+                <h4>Semaforo anterior</h4>
+                <p>{props.lastTrafficLight}</p>
+              </div>
             </div>
-            <div className="fecha-de-cambio">
-              <h4>Ultima fecha de cambio</h4>
-              <p>{props.dateOfChange}</p>
+            {/* div semaforo ends */}
+
+            {/* div data starts */}
+            <div id="wrapper" className="data-padding">
+              <div>Casos COVID-19<div>6325</div></div>
+              <div>Aumento de casos COVID-19<div>1520</div></div>
+              <div>Defunciones<div>1200</div></div>
+              <div>Aumento defunciones<div>320</div></div>
             </div>
-            <div className="semaforo-anterior">
-              <h4>Semaforo anterior</h4>
-              <p>{props.lastTrafficLight}</p>
-            </div>
-            <button onClick={props.onClose} className="button">
-              Close
-            </button>
+            {/* div data ends */}
           </div>
+          {/* div modal footer ends */}
         </div>
       </div>
     </CSSTransition>,
